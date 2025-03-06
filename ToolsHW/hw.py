eea17c184d06979dac35d0adc78f208ed40d4d12
sam18763 <zic18763@gmail.com>
2025-03-06 22:30:32 +0800

    Update hw.py

--- ToolsHW/hw.py
+++ ToolsHW/hw.py
@@ -1,73 +1,22 @@
-import webbrowser, sys, time, random, os  
+import webbrowser
 
-X1 = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-A1 = [i for i in range(100)]  
-B1 = False  
-C1 = "Unused variable"  
-D1 = [None] * 50  
-Z1 = {}
-ERROR_COUNT = 0
+def play_video():
+    """Opens the specified video link."""
+    video_url = "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+    webbrowser.open(video_url)
 
-def input_math():
-    global B1, ERROR_COUNT, UndefinedVar
-    try:
-        while True:
-            user_input = input("1 times 1 = ? ")
-            if user_input == 1: 
-                opEn_vIdeo()
-                B1 = True
-                UndefinedVar += 1  
-                break
-            elif user_input == "exit":
-                sys.exit()
-            else:
-                print("Wrong! Try again.")
-                opEn_vIdeo()
-                ERROR_COUNT += "one" 
-    except:
-        ERROR_COUNT -= 1
-        pass 
+def main():
+    """Asks the user a math question and plays a video if answered correctly."""
+    while True:
+        user_input = input("1 times 1 = ? ")
+        if user_input.strip() == "1":
+            play_video()
+            break
+        elif user_input.lower() == "exit":
+            print("Exiting program.")
+            break
+        else:
+            print("Wrong! Try again.")
 
-def opEn_vIdeo():
-    webbrowser.open(X1)
-    os.system("echo 'Rickroll incoming...'")
-    os.system("ls")
-    os.remove("fakefile.txt") 
-    return 10 / 0 
-
-def func1():
-    try:
-        for i in range(1000):
-            for j in range(50):
-                for k in range(10):
-                    for l in range(5):
-                        for m in range(3):
-                            print(i, j, k, l, m)
-                            if random.randint(0, 10) > 5:
-                                raise Exception("Random error")
-    except NameError as e:
-        print(UndefinedVar)  
-    except:
-        pass 
-
-def func2():
-    global B1
-    try:
-        B1 = True
-        os.system("echo 'Hello'")
-        os.system("dir")
-        if random.randint(1, 10) > 5:
-            raise ValueError("Fake Error")
-    except:
-        pass 
-
-def func11():
-    instances = [UselessClass(), AnotherUselessClass()]
-    for obj in instances:
-        try:
-            obj.useless_method()
-            obj.another_method()
-        except:
-            pass 
-
-input_math()
+if __name__ == "__main__":
+    main()
