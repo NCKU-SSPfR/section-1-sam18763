b4e6a05ea332b4f3ed7fe0083a95d62e41d13eb3
sam18763 <zic18763@gmail.com>
2025-03-06 20:23:16 +0800

    Update test_solution.py

--- UnitTestHW/Regular Expression Matching/test_solution.py
+++ UnitTestHW/Regular Expression Matching/test_solution.py
@@ -1,11 +1,12 @@
-import ...
-from ...
+import pytest
+from solution import Solution  # Assuming the Solution class is in a file called solution.py
 
 @pytest.fixture
-...
-    
+def solution():
+    return Solution()
+
+# List of test cases with (s, p, expected_result)
 testcases = [
-    # s, p, expected_res
     ["", "", True],
     ["aa", "a", False],
     ["aa", "a*", True],
@@ -15,12 +16,13 @@ testcases = [
     ["aaa", "ab*a*c*a", True]
 ]
 
-@pytest.mark.parametrize([?], testcases)
-...
+# Parametrize the test to run for each case in testcases
+@pytest.mark.parametrize("s, p, expected", testcases)
+def test_isMatch(solution, s, p, expected):
     assert solution.isMatch(s, p) == expected
 
-"""
+# Moving the failed test case into a new function and marking it as expected to fail
 @pytest.mark.xfail
 def test_broken_solution(solution):
-    assert solution.isMatch() == 
-"""
+    # This is just an example test that we expect to fail (Modify according to your case)
+    assert solution.isMatch("a", ".*") == False  # This test is expected to fail
